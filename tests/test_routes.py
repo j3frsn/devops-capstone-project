4f11cb1d369f6b25f4639308686a1e2fe8b623ff
Jeferson Andrade <j3frsn@proton.me>
2025-01-10 16:23:26 -0500

    Added route+test for deleting an account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,3 +163,9 @@ class TestAccountService(TestCase):
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
   
+    def test_delete_an_account(self):
+        "It should delete an existing account"
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
